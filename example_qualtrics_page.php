d4cb6d4a8e7e5f3672b471f0229446a4fc9d3983
Jacob Levernier <jleverni@uoregon.edu>
2014-09-21 18:42:45 -0700

    Initial Commit for public repository.

---
+++ example_qualtrics_page.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+</head>
+
+<body>
+
+    <?php // This just says that this is the start of a block of PHP code.
+
+        require "qualtrics_graph_helper.php"; // This loads the code from the file.
+        
+        load_highcharts_code(); // This takes the file loaded above and prints out a bit of code necessary to make the charts work later on.
+
+
+/////////////////////////////////////////////////////////////
+// A few notes, before we get started:
+    // The language we're using here is called PHP.
+    // Variables in PHP have a dollar sign in front of them. For example, $example is a variable.
+    // Every line of code has to have a semicolon (;) at the end of it, to show that that's the end of the line.
+    // We're going to use the code that we loaded above and use it to create a variable. We can get at the data within that variable with an arrow. For example, $variable_name->option_name = "whatever"
+    // Your Qualtrics survey needs to be set up to redirect the user to this page upon survey completion, and for that redirect to also tell this page what the user's Qualtrics Response ID is. To do this, go in to edit the survey within Qualtrics, and click "Survey Flow." Add an "End of Survey" element at the end of the survey flow. In that "End of Survey" element, click "Customize," and choose "Redirect to a URL". The URL should be, e.g., (what's important is at the end) http://www.example.com/example_qualtrics_page.php?id=${e://Field/ResponseID}, where "http://www.example.com/example_qualtrics_page.php" is replaced with the address of this page.
+// Ok, we're ready to start!
+/////////////////////////////////////////////////////////////
+
+
+        // First, we'll set up a new container for the options for our Qualtrics data (this draws on the code loaded above, which spells out what "QualtricsDataProcessor" is):
+        $example = new QualtricsDataProcessor;
+
+        // You should now edit a few Qualtrics-related options from our new holder variable:
+        $example->qualtrics_options["User"] = 'your_Qualtrics_username_goes_here'; // This is your official Qualtrics username. You can find it by logging into Qualtrics, then clicking on your username, and then "Account Settings." You'll see your username under "Recent Logins"
+            // NOTE: Once you start using this code in your web page, you may see extra records under "Recent Logins." If you see that happening, it's just from your web page going to get the data from Qualtrics.
+
+        $example->qualtrics_options["Token"] = "your_token_goes_here"; // This is like a password for being able to go and get data from Qualtrics for surveys that you created with your Qualtrics account. Qualtrics generates it randomly. You can get your Qualtrics Token by going in Qualtrics through Account Settings -> Qualtrics IDs -> Generate Token. NOTE that if you code ever gets released accidentally, you can go get a new token through that Qualtrics page.
+
+        $example->qualtrics_options["SurveyID"] = "your_survey_id_goes_here"; // You can get the ID number of your survey by going in Qualtrics through Account Settings -> Qualtrics IDs -> Generate Token.
+
+        // Additional Note: You can edit whatever other Qualtrics options from https://survey.qualtrics.com/WRAPI/ControlPanel/docs.php#getLegacyResponseData_2.3 that you like, using the format above.
+
+
+        // Now we'll go and actually get the data from Qualtrics:
+        $data_from_qualtrics = $example->get_qualtrics_data(); // It's as easy as that!
+
+        // Now that we have the data, let's define some subscales. You can indicate question names from the Quatrics survey by enclosing the names in curly braces (for example, "{Question1} + {Question5}"). You can define multiple subscales using brackets, like this: $example->subscale_equations["subscale_name"] = "{Question1} + {Question5}";
+
+        $example->subscale_equations["Alpha"] = "{SDI_46_9} + {SDI_46_16} + {SDI_46_22} + {SDI_46_32} - {SDI_46_3} - {SDI_46_18} - {SDI_46_25} - {SDI_46_37}";
+
+        $example->subscale_equations["Beta"] = "{SDI_46_6} + {SDI_46_10} + {SDI_46_23} + {SDI_46_41} + {SDI_46_43} + {SDI_46_45} - {SDI_46_14} - {SDI_46_31} - {SDI_46_33} - {SDI_46_39}";
+
+        $example->subscale_equations["Gamma"] = "{SDI_46_5} + {SDI_46_11} + {SDI_46_13} + {SDI_46_17} + {SDI_46_21} + {SDI_46_24} + {SDI_46_26} - {SDI_46_28} - {SDI_46_30} - {SDI_46_34}";
+
+        $example->subscale_equations["Delta"] = "{SDI_46_2} + {SDI_46_19} + {SDI_46_29} + {SDI_46_38} - {SDI_46_7} - {SDI_46_15} - {SDI_46_35} - {SDI_46_42}";
+
+        $example->subscale_equations["Epsilon"] = "{SDI_46_4} + {SDI_46_8} + {SDI_46_12} + {SDI_46_20} + {SDI_46_27} + {SDI_46_40} - {SDI_46_1} - {SDI_46_36} - {SDI_46_44} - {SDI_46_46}";
+
+
+        // Now we can have Qualtrics actually score everything. This step just processes the subscale equations that we defined above:
+
+        $scored_subscales = $example->score_data_subscales($data_from_qualtrics, $example->subscale_equations);
+
+
+        // Having scored the data, we can now get the data of the current user, if we want (the column name that we want to look for is called "ResponseID," which is what Qualtrics calls the ID number of each participant. The ID number of the current user (sent over from the Qualtrics survey) is automatically saved in $example->response_id_of_current_user):
+
+        $scored_data_of_specific_current_user = $example->get_specific_row($scored_subscales,"ResponseID", $example->response_id_of_current_user);
+
+
+        // We can break the data down into subscales:
+
+        $Alpha_data_of_current_user = $scored_data_of_specific_current_user['Alpha'];
+        $Beta_data_of_current_user = $scored_data_of_specific_current_user['Beta'];
+        $Gamma_data_of_current_user = $scored_data_of_specific_current_user['Gamma'];
+        $Delta_data_of_current_user = $scored_data_of_specific_current_user['Delta'];
+        $Epsilon_data_of_current_user = $scored_data_of_specific_current_user['Epsilon'];
+
+
+        // We can also get the average across all users for a column (if you like, you can also use get_sum_of_column... instead of get_average_of_column...):
+
+        $average_Alpha_subscale = $example->get_average_of_column($scored_subscales, "Alpha");
+        $average_Beta_subscale = $example->get_average_of_column($scored_subscales, "Beta");
+        $average_Gamma_subscale = $example->get_average_of_column($scored_subscales, "Gamma");
+        $average_Delta_subscale = $example->get_average_of_column($scored_subscales, "Delta");
+        $average_Epsilon_subscale = $example->get_average_of_column($scored_subscales, "Epsilon");
+
+/* 
+Now we're ready to make the actual graph! To do that, we'll be using HighCharts (http://www.highcharts.com/demo/). HighCharts is easy to use, and there are even tools to automatically create most of the code that we'll need.
+
+To create HighCharts code, I recommend doing three things:
+    1) Go to http://www.highcharts.com/demo/, look up the type of graph that you want to create (pie chart, bar graph, etc.), and click "View Options." Copy the code that pops up.
+    2) Paste that example code below, between the quotation marks for the $code_for_our_graph variable. 
+    3) Go through the code that you pasted below and just change the options in it. For the data, you can use the variables that we defined above!
+       This might seem daunting, but HighCharts is actually really easy to understand just by looking at it.
+*/
+
+$code_for_our_graph = "
+        $(function () {
+                $('#graph_from_qualtrics_data').highcharts({
+                    chart: {
+                        type: 'bar'
+                    },
+                    title: {
+                        text: 'Your Score vs. the Average Score'
+                    },
+                    subtitle: {
+                        text: 'for the SDI-46'
+                    },
+                    xAxis: {
+                        categories: ['Tradition-oriented Religiousness (Alpha)', 'Unmitigated Self-Interest (Beta)', 'Communal Rationalism (Gamma)', 'Subjective Spirituality (Delta)', 'Egalitarianism (Epsilon)'],
+                        title: {
+                            text: null
+                        }
+                    },
+                    yAxis: {
+                        min: -10,
+                        max: 15,
+                        title: {
+                            text: 'SDI-46 Subscale Score',
+                            align: 'middle'
+                        },
+                        labels: {
+                            overflow: 'justify'
+                        }
+                    },
+                    tooltip: {
+                        valueSuffix: ' SDI-46 scale points'
+                    },
+                    plotOptions: {
+                        bar: {
+                            dataLabels: {
+                                enabled: false
+                            }
+                        }
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'top',
+                        x: -40,
+                        y: 100,
+                        floating: true,
+                        borderWidth: 1,
+                        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor || '#FFFFFF'),
+                        shadow: true
+                    },
+                    credits: {
+                        enabled: false
+                    },
+                    series: [{
+                        name: 'Average Score',
+                        data: [$average_Alpha_subscale, $average_Beta_subscale, $average_Gamma_subscale, $average_Delta_subscale, $average_Epsilon_subscale]
+                    }, {
+                        name: 'Your Score',
+                        data: [$Alpha_data_of_current_user, $Beta_data_of_current_user, $Gamma_data_of_current_user, $Delta_data_of_current_user, $Epsilon_data_of_current_user]
+                    }]
+                });
+            });
+";
+
+
+
+// Tell PHP to print out the text below (the user's web browser will see this code and turn it into a graph):
+print "<script>$code_for_our_graph</script>";
+
+
+// NOTE: Now, with all of this code written, just put the following code wherever in your webpage you want your graph to show up (for this example, I've just put it below -- this will produce a page that is blank aside from the graph):
+//    <div id="graph_from_qualtrics_data" style="min-width: 310px; max-width: 800px; height: 400px; margin: 0 auto"></div>
+
+// Close the PHP part of the code:
+    ?>
+
+    <div id="graph_from_qualtrics_data" style="min-width: 310px; max-width: 800px; height: 400px; margin: 0 auto"></div>
+
+
+
+</body>
+</html>
---
+++ qualtrics_graph_helper.php
@@ -0,0 +1,322 @@
+<?php
+
+function load_highcharts_code() { // This just prints out the code for HighCharts (the idea here is to make it easier for non-coders to use).
+    echo '
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+        <script src="http://code.highcharts.com/highcharts.js"></script>
+        ';
+    return TRUE;
+}
+
+class QualtricsDataProcessor
+{
+
+    public $qualtrics_options = array(
+      "Request" => "getLegacyResponseData",
+      "User" => "", // Your Qualtrics username. Down below, we will urlencode this so that the '#' becomes '%23' (which is its html url-safe equivalent).
+      "API_SELECT" => "ControlPanel",
+      "Version" => "2.3",
+      "Token" => "", // You can get your Qualtrics Token through Account Settings -> Qualtrics IDs -> Generate Token.
+      "Format" => "JSON",
+      "SurveyID" => "" // The Qualtrics Survey ID
+    );
+
+
+    public $response_id_of_current_user;
+    function __construct() // This is here because of the advice of http://stackoverflow.com/a/16761755/1940466 (cf. http://stackoverflow.com/a/455929/1940466), which notes that within a PHP class, dynamic variables must be defined within functions (this solves an error that I received otherwise, either using `$response_id_of_current_user = $_GET['id'];` or `var $response_id_of_current_user = $_GET['id'];`)
+        {
+            $this->response_id_of_current_user = $_GET['id']; // This is supplied from Qualtrics through a $_GET variable. To get Qualtrics to provide it, edit the survey, then edit Survey Flow. Add an "End of Survey" block, click "Customize," and choose "Redirect to a URL". The URL should be, e.g., (what's important is at the end) http://www.example.com/scratchpad.php?id=${e://Field/ResponseID}
+        }
+
+
+    function get_qualtrics_data($qualtrics_parameters=NULL) // See the beginning of the function for the reason for the NULL default here.
+     {
+        // Following the advice of http://stackoverflow.com/a/5859412/1940466, if the user hasn't explicitly passed any parameters, we'll assign a default (we do it this way here because default options can't be variables):
+        if(is_null($qualtrics_parameters))
+            {
+                $qualtrics_parameters = $this->qualtrics_options;
+            };
+    
+        // NOTE: curl must be *enabled* in PHP before it can be used. Check that it is with:
+        //phpinfo();
+
+        $base_api_url = "https://survey.qualtrics.com/WRAPI/ControlPanel/api.php"; // This should NOT have a question mark or any GET parameters at the end of it. Those will be added below.
+
+        // The array elements below should be "key"=>"value" where "key" is a GET request parameter for the Qualtrics API (e.g., "User" or "Token"):
+        if(!isset($qualtrics_parameters) || !is_array($qualtrics_parameters)) // If the user didn't give URL parameters to use.
+            {
+                echo nl2br('
+                <b>Error:</b> When using the get_qualtrics_data() function, you must insert an array of options in the parentheses. Here\'s an example:
+
+                    $parameters = array(
+                      "Request" => "getLegacyResponseData",
+                      "User" => "jleverni%23oregon", // Enter your UO username in place of \'jleverni\'
+                      "API_SELECT" => "ControlPanel",
+                      "Version" => "2.3",
+                      "Token" => "M8yw5LoGf0X1oQL5RxmWi6tjU80TAScNG4PaeNVl", // You can get your Qualtrics Token through Account Settings -> Qualtrics IDs -> Generate Token.
+                      "Format" => "JSON",
+                      "SurveyID" => "SV_eaK7DqH8d7DvDYV"
+                    );
+                    
+                $data = get_qualtrics_data($parameters);
+
+                ');
+                    
+                    return FALSE; // Exit the function.
+            }; // End if statement
+
+        // If nothing has caused us to exit so far, we'll keep going:
+        
+        $combined_url_GET_parameters = ""; // Just giving this an initial value here.
+        // Combine the URL GET parameters to be suitable for a URL:
+        // Following the advice at http://stackoverflow.com/a/13789336, set a marker to tell us later whether we're seeing the first element of the array:
+            $first = true;
+
+        foreach($qualtrics_parameters as $key => $value) {
+            if($first) { // If we're looking at the first GET parameter (i.e., the first element of the array), add a "?" to the beginning (this is how GET requests work). If not, we'll add "&" to the beginning.
+                $combined_url_GET_parameters = $combined_url_GET_parameters . "?";
+                $first = false;
+            }
+            else {
+                $combined_url_GET_parameters = $combined_url_GET_parameters . "&";
+            }
+
+            $combined_url_GET_parameters = $combined_url_GET_parameters . "$key=" . urlencode($value);
+        };
+
+        $full_qualtrics_url_including_get_parameters = $base_api_url . $combined_url_GET_parameters;
+
+        // echo "Full url is $full_qualtrics_url_including_get_parameters <br />"; // Good for debugging purposes.
+
+        $curl_request = curl_init();
+        curl_setopt($curl_request, CURLOPT_URL, $full_qualtrics_url_including_get_parameters);
+        curl_setopt($curl_request, CURLOPT_RETURNTRANSFER, 1); // This gets curl to return (instead of print out) the response.
+
+        // Execute the curl request:
+        $curl_response = curl_exec($curl_request);
+        curl_close($curl_request);
+
+        // var_dump($curl_response); // Good for diagnostic purposes.
+
+        // Parse the curl_response object into an associative array (this assumes that the curl object "Format" API parameter above is set to "JSON":
+        $parsed_curl_response = json_decode($curl_response, $assoc = true);
+
+        // Return the data:
+        return $parsed_curl_response;
+
+    } // End function definition
+
+
+    // From http://www.php.net/manual/en/function.array-column.php -- array_column() requires PHP 5.5 or higher. Otherwise, one can define a homemade function like this:
+        // From http://stackoverflow.com/a/19188062
+    function array_column($array, $column){
+        $a2 = array();
+        array_map(function ($a1) use ($column, &$a2){
+            array_push($a2, $a1[$column]);
+        }, $array);
+        return $a2;
+
+    // SOME EXAMPLES:
+    // If we wanted to get the average for column $foo, we could use this:
+        // $full_foo_column = array_column($parsed_curl_response, 'foo');
+        // $foo_average = array_sum($full_foo_column) / count($full_foo_column);
+
+    // To exclude blank responses, we can follow the advice at http://www.php.net/manual/en/function.array-filter.php#111091 and use array_filter( $array, 'strlen' );
+
+        // An example of the above:
+            //    $full_SDI_46_1_column_excluding_blank_responses = array_filter(array_column($parsed_curl_response, 'SDI_46_1'), 'strlen');
+            //    $SDI_46_1_average = array_sum($full_SDI_46_1_column_excluding_blank_responses) / count($full_SDI_46_1_column_excluding_blank_responses);
+            //    echo "The average of the columns is $SDI_46_1_average";
+
+    } // End function definition
+
+
+    function score_data_subscales($data_from_qualtrics, $array_of_subscale_equations) {
+
+        if(!isset($data_from_qualtrics)) // If the user hasn't supplied any data...
+            {
+                echo nl2br('
+                    <br>Error:</b> When you use the score_data_subscales() function, you must include the data from Qualtrics as the first parameter. Here\'s an example:
+                    
+                    scored_subscales = score_data_subscales($data_from_qualtrics);
+                    
+                ');
+                
+                return FALSE;
+            };
+
+
+        if(!isset($array_of_subscale_equations) || !is_array($array_of_subscale_equations)) // If the user hasn't defined what the subscales are...
+            {
+                echo nl2br('
+                    <br>Error:</b> When you use the score_data_subscales() function, you must insert an array of options in the parentheses. Here\'s an example (put the Qualtrics variable names you want to use in curly brackets):
+
+                    $subscale_equations = array(
+                    
+                        "Alpha" => "{SDI_46_9} + {SDI_46_16} + {SDI_46_22} + {SDI_46_32} - {SDI_46_3} - {SDI_46_18} - {SDI_46_25} - {SDI_46_37}",
+
+                        "Beta" => "{SDI_46_6} + {SDI_46_10} + {SDI_46_23} + {SDI_46_41} + {SDI_46_43} + {SDI_46_45} - {SDI_46_14} - {SDI_46_31} - {SDI_46_33} - {SDI_46_39}",
+
+                        "Gamma" => "{SDI_46_5} + {SDI_46_11} + {SDI_46_13} + {SDI_46_17} + {SDI_46_21} + {SDI_46_24} + {SDI_46_26} - {SDI_46_28} - {SDI_46_30} - {SDI_46_34}",
+
+                        "Delta" => "{SDI_46_2} + {SDI_46_19} + {SDI_46_29} + {SDI_46_38} - {SDI_46_7} - {SDI_46_15} - {SDI_46_35} - {SDI_46_42}",
+
+                        "Epsilon" => "{SDI_46_4} + {SDI_46_8} + {SDI_46_12} + {SDI_46_20} + {SDI_46_27} + {SDI_46_40} - {SDI_46_1} - {SDI_46_36} - {SDI_46_44} - {SDI_46_46}"
+                    );
+                    
+                    scored_subscales = score_data_subscales($data_from_qualtrics, $subscale_equations);
+                
+                ');
+                
+                return FALSE;
+            }
+            else // If we DO have definitions for the subscales, add an additional key-value pair:
+                {
+                    if(!empty($array_of_subscale_equations["ResponseID"]))
+                        {
+                            echo "<b>Error:</b> Please do not use 'ResponseID' as a key in your array. The script uses that name automatically.<br/>";
+                            
+                            exit(); // This is potentially a catastrophic error, so we'll exit the whole script.
+                        };
+
+            }; // End if statement.
+
+        $array_of_scored_questionnaire_data = array(); // Create a blank array (we'll populate it in a moment).
+
+        foreach ($data_from_qualtrics as $key => $row) {
+            // To get the SDI_46_1 column for this row, we would use $row["SDI_46_1"]
+
+            $filled_in_equation_for_this_row = array(); // Wipe this variable so that it's a blank slate below.
+
+            foreach($array_of_subscale_equations as $equation_key => $equation_row) {
+
+                $filled_in_equation_for_this_row["$equation_key"] = preg_replace_callback(
+                    '|{(.*?)}|',
+                    function($variable_name) use($key, $row, $equation_key, $equation_row) {
+                        $variable_name_of_interest = $variable_name[1];
+
+                        if($row["$variable_name_of_interest"] != "") // If this is not blank
+                            {
+                                $value_of_variable_of_interest = $row["$variable_name_of_interest"];
+                            }
+                        else // If this IS blank, set it to 0
+                            {
+                                $value_of_variable_of_interest = 0;
+                            };
+                        
+                        return $value_of_variable_of_interest;
+                    },
+                    $equation_row);
+                
+                    $scored_data_for_this_row["$equation_key"] = eval("return " . $filled_in_equation_for_this_row["$equation_key"] . ";"); // NOTE WELL: This uses eval(), which can be very dangerous if it ever touches user input (since the user could give a malicious string to be evaluated). However, in this case, the user *already is expected to have access to the PHP code,* so we're not giving them any more power than they already have. **But DO NOT** make this class accessible through a form or something like that, since eval() would then be very dangerous to use.
+
+                };
+
+            $scored_data_for_this_row["ResponseID"] = $key; // This makes sense with the preg_replace_callback() below. NOTE WELL that the way that Qualtrics\' API seems to work is to return an array NAMED BY ResponseID, with all of the data within each ResponseID-named row. That\'s why this works.
+
+            // Add the scored subscales to the full array of all participants' scored subscales:
+            array_push($array_of_scored_questionnaire_data, $scored_data_for_this_row);
+
+            };
+
+        // If nothing else has stopped us at this point, we'll return the scored subscales:
+
+        return $array_of_scored_questionnaire_data;
+
+    } // End function definition.
+
+
+
+
+
+    // AN IDEA FOR ABSTRACTING THIS:
+    // I could make it so that the array above is all that needs to be set by the user (using an include() statement).
+    // Then automatically calculate average subscale results using a foreach() loop over that array.
+    // So the user would just need to set that array, and, say, the graph model below (again, I can use an include() statement there) (since the user will probably want to customize it).
+    // I could, though, also do a loop within the model creation script. Or just have example text for model creation.
+
+
+
+
+
+    function get_average_of_column($data, $column_name) {
+
+        if(!isset($data)) // If the user hasn't supplied any data...
+            {
+                echo nl2br('
+                    <br>Error:</b> When you use the get_average_of_column() function, you must include some data (either the raw data, or the scored data) from Qualtrics as the first parameter. Here\'s an example:
+                    
+                    scored_subscales = score_data_subscalesget_sum_of_column($scored_data_from_qualtrics, "Column_name_from_Qualtrics");
+                    
+                ');
+                
+                return FALSE;
+            };
+        
+        
+            if(!isset($column_name)) // If the user hasn't supplied any data...
+            {
+                echo nl2br('
+                    <br>Error:</b> When you use the get_average_of_column() function, you must include the column name from Qualtrics as the second parameter. Here\'s an example:
+                    
+                    scored_subscales = score_data_subscales($data_from_qualtrics, "Column_name_in_quotes");
+                    
+                ');
+                
+                return FALSE;
+            };
+            
+        $computed_average = array_sum($this->array_column($data, "$column_name")) / count($this->array_column($data, "$column_name"));
+        
+        return $computed_average;
+
+    } // End function definition
+
+
+    function get_sum_of_column($data, $column_name) {
+
+        if(!isset($data_from_qualtrics)) // If the user hasn't supplied any data...
+            {
+                echo nl2br('
+                    <br>Error:</b> When you use the get_sum_of_column() function, you must include some data (either the raw data, or the scored data) from Qualtrics as the first parameter. Here\'s an example:
+                    
+                    scored_subscales = get_sum_of_column($scored_data_from_qualtrics, "Column_name_from_Qualtrics");
+                    
+                ');
+                
+                return FALSE;
+            };
+        
+        
+            if(!isset($column_name)) // If the user hasn't supplied any data...
+            {
+                echo nl2br('
+                    <br>Error:</b> When you use the get_sum_of_column() function, you must include the column name from Qualtrics as the second parameter. Here\'s an example:
+                    
+                    scored_subscales = score_data_subscales($data_from_qualtrics, "Column_name_in_quotes");
+                    
+                ');
+                
+                return FALSE;
+            };
+            
+        $computed_sum = array_sum($this->array_column($data, "$column_name"));
+        
+        return $computed_sum;
+
+    } // End function definition
+
+    // Get the current user's data:
+    // Per http://stackoverflow.com/a/12377045, there doesn't seem to be a built-in PHP function for searching an associative array, so we'll build one:
+    function get_specific_row($array, $field_name_to_match, $search_term) {
+        foreach ($array as $row)
+            {
+                if ($row[$field_name_to_match] == $search_term) {
+                    return $row;
+                };
+            };
+    }
+
+} // End class definition
+
+?>
